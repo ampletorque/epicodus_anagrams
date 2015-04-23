80a1c7803c51f89aad597effa6aa80329ce5adad
Jim Patt & andrew david burt <student@epicodus.com>
2015-04-23 14:58:09 -0700

    first integration specrspec

--- spec/anagram_int_spec.rb
+++ spec/anagram_int_spec.rb
@@ -6,7 +6,19 @@ set(:show_exceptions, false)
 describe('checks to see if a list of words are anagrams of one word', {:type => :feature}) do
   it("returns a lits of words that are anagrans to one word") do
     visit('/')
-    fill_in('anagrammable' , :with => "top")
+    fill_in('anagramm
+    <html>
+    <head>
+    <title>This is COIN. COUNT. </title>
+    <link rel='stylesheet' href='https://maxcdn.bootstrap.cdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>
+    </head>
+    <body>
+    <div class="container">
+    <%= yield %>
+    </div>
+    </body>
+    </html>
+    able' , :with => "top")
     click_button('Check It!')
     expect(page).to have_content(("top, opt"))
   end
